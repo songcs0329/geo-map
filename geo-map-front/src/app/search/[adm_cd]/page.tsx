c6758a0fff8a9aae95953245257d78c71fd1fb42
songcs0329 <songcs0329@gmail.com>
2026-02-03 04:25:37 +0900

    update hydration query

--- geo-map-front/src/app/search/[adm_cd]/page.tsx
+++ geo-map-front/src/app/search/[adm_cd]/page.tsx
@@ -1,4 +1,10 @@
 import SearchDrawer from "@/components/pages/search/SearchDrawer";
+import { geoJSONRegionByAdmCdQueryOptions } from "@/hooks/useGetGeoJSONRegionByAdmCd";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 type Params = Promise<{ adm_cd: string }>;
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
@@ -10,7 +16,15 @@ type SearchDetailPageProps = {
 
 export default async function SearchDetailPage(props: SearchDetailPageProps) {
   const params = await props.params;
-  const searchParams = await props.searchParams;
 
-  return <SearchDrawer adm_cd={params.adm_cd} />;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(
+    geoJSONRegionByAdmCdQueryOptions(params.adm_cd)
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <SearchDrawer adm_cd={params.adm_cd} />;
+    </HydrationBoundary>
+  );
 }
--- geo-map-front/src/hooks/useGetGeoJSONRegionByAdmCd.tsx
+++ geo-map-front/src/hooks/useGetGeoJSONRegionByAdmCd.tsx
@@ -1,16 +1,20 @@
 import { getGeoJSONRegionByAdmCd } from "@/lib/apis/geojson";
-import { useQuery } from "@tanstack/react-query";
+import { queryOptions, useQuery } from "@tanstack/react-query";
+
+export const geoJSONRegionByAdmCdQueryOptions = (adm_cd: string) => {
+  return queryOptions({
+    queryKey: ["region", adm_cd],
+    queryFn: () => getGeoJSONRegionByAdmCd(adm_cd),
+    enabled: !!adm_cd,
+  });
+};
 
 function useGetGeoJSONRegionByAdmCd(adm_cd: string) {
   const {
     data: region,
     isLoading,
     error,
-  } = useQuery({
-    queryKey: ["region", adm_cd],
-    queryFn: () => getGeoJSONRegionByAdmCd(adm_cd),
-    enabled: !!adm_cd,
-  });
+  } = useQuery(geoJSONRegionByAdmCdQueryOptions(adm_cd));
 
   return {
     region: region ?? null,
