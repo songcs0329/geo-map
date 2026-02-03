190f9021c40b580737cda26a10551eaf6242950e
songcs0329 <songcs0329@gmail.com>
2026-02-03 17:58:28 +0900

    update hello world

--- geo-map-server/src/app.module.ts
+++ geo-map-server/src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 import { HealthModule } from './health/health.module';
 import { GeoJSONModule } from './geojson/geojson.module';
 import { SearchModule } from './search/search.module';
 
 @Module({
+  controllers: [AppController],
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : undefined,
+      envFilePath: process.env.NODE_ENV
+        ? `.env.${process.env.NODE_ENV}`
+        : undefined,
       ignoreEnvFile: process.env.NODE_ENV === 'production',
     }),
     HealthModule,
